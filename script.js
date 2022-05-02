80a70fae00d9be9eab43378a634122506879c104
Carl Justine F. Malabanan <carljustinem984@gmail.com>
2022-05-02 17:56:49 +0800

    New Files

---
+++ script.js
@@ -0,0 +1,18 @@
+function openKofi() {
+  window.open("https://ko-fi.com/jhellydraws");
+}
+function opentwt() {
+  window.open("https://www.twitter.com/jhellydraws");
+}
+function openHome() {
+  window.location.href = "index.html?backurl" + window.location.href;
+}
+function openPrice() {
+  window.location.href = "price.html?backurl" + window.location.href;
+}
+function openTos() {
+  window.location.href = "tos.html?backurl" + window.location.href;
+}
+function openContact() {
+  window.location.href = "contact.html?backurl" + window.location.href;
+}
