5691e8962ab4aab8e4d4cbbb2202b769d86ab035
Carl Justine F. Malabanan <carljustinem984@gmail.com>
2022-08-06 16:59:52 +0800

    Update script.js
    Logs

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 function openKofi() {
-  window.open("https://ko-fi.com/jhellydraws");
+  var a = window.open("https://ko-fi.com/jhellydraws");
+  a;
+  console.log(a);
 }
 function opentwt() {
-  window.open("https://www.twitter.com/jhellydraws");
+  var b = window.open("https://www.twitter.com/jhellydraws");
+  b;
+  console.log(b);
 }
 function openHome() {
-  window.location.href = "index.html?backurl" + window.location.href;
+  var c = (window.location.href = "index.html?backurl" + window.location.href);
+  c;
+  console.log(c);
 }
 function openPrice() {
-  window.location.href = "price.html?backurl" + window.location.href;
+  var d = (window.location.href = "price.html?backurl" + window.location.href);
+  d;
+  console.log(d);
 }
 function openTos() {
-  window.location.href = "tos.html?backurl" + window.location.href;
+  var e = (window.location.href = "tos.html?backurl" + window.location.href);
+  e;
+  console.log(e);
 }
 function openContact() {
-  window.location.href = "contact.html?backurl" + window.location.href;
+  var f = (window.location.href =
+    "contact.html?backurl" + window.location.href);
+  f;
+  console.log(f);
 }
